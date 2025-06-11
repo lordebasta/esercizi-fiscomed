1a01f88f6ef43ec9e8bec3a2af554e67ef1f3770
lordebasta <lord@eve>
2025-06-11 18:30:33 +0200

    setup pg dbs, test and dev, add validation for Task model and test

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_06_11_084105) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.text "title", null: false
     t.text "description"
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save task without title" do
+    task = Task.new
+    assert_not task.save, "Saved the task without a title"
+  end
 end
